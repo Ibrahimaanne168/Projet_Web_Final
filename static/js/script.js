7c0013eda1a47b13c9de348e25d9c9a97ab4dd80
syllaamy1-crypto <sylla.amy1@.edu.sn>
2026-07-08 12:07:44 +0000

    mis a jour des pages du dossier templates , du css et du js

--- static/js/script.js
+++ static/js/script.js
@@ -33,5 +33,71 @@ setInterval(() => {
     showSlide(current);
 }, 5000);
 
-/*javascript du galerie */
+function ouvrirPopupDepuisCarte(card) {
+    document.getElementById("popup-nom").innerText = card.dataset.nom;
+    document.getElementById("popup-grade").innerText = card.dataset.grade;
+    document.getElementById("popup-departement").innerText = card.dataset.departement;
+    document.getElementById("popup-email").innerText = card.dataset.email;
+    document.getElementById("popup-email").href = "mailto:" + card.dataset.email;
+    document.getElementById("popup-recherche").innerText = card.dataset.recherche;
+    document.getElementById("popup-photo").src = card.dataset.photo;
+
+document.getElementById("popup-prof").classList.add("active");}
+
+function fermerPopup(){
+
+    document.getElementById("popup-prof").classList.remove("active");
+
+}
+    setTimeout(() => {
+        const flashes = document.querySelectorAll('.flash');
+        flashes.forEach(f => {
+            f.style.transition = "0.5s";
+            f.style.opacity = "0";
+            setTimeout(() => f.remove(), 500);
+        });
+    }, 3000);
+
+    function showTab(id){
+
+    document.querySelectorAll('.tab').forEach(t=>{
+        t.style.display='none';
+    });
+
+    document.getElementById(id).style.display='block';
+}
+
+function afficherModificationPhoto(id){
+
+    let form = document.getElementById(
+        "modifier-photo-" + id
+    );
+
+    if(form.style.display === "none"){
+        form.style.display = "block";
+    }
+    else{
+        form.style.display = "none";
+    }
+
+}
+
+
+function afficherAjoutPhoto(id){
+
+let form=document.getElementById(
+"ajout-photo-"+id
+);
+
+
+form.style.display =
+form.style.display==="none"
+?
+"block"
+:
+"none";
+
+}
+
+
 
